597c9f9358ae5b64ac6d36c3a5a246d1c9dc0b4e
rsookram <r@sookram>
2013-07-16 21:04:34 -0400

    Added side swipe to dismiss quote dialog

--- src/com/merono/g/QuoteDialogFragment.java
+++ src/com/merono/g/QuoteDialogFragment.java
@@ -5,12 +5,15 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class QuoteDialogFragment extends DialogFragment {
+public class QuoteDialogFragment extends DialogFragment implements GestureDetector.OnGestureListener {
     private static ArrayList<Post> posts;
+    private GestureDetector mDetector;
 
     public static QuoteDialogFragment newInstance(Post post, ArrayList<Post> allPosts) {
         ArrayList<String> quoteIds = post.getQuoteIds();
@@ -21,11 +24,55 @@ public class QuoteDialogFragment extends DialogFragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDetector = new GestureDetector(getActivity(), this);
+    }
+
+    @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         Activity activity = getActivity();
-        ListView lv = new ListView(activity);
+        ListView lv = new ListView(activity) {
+            @Override
+            public boolean onTouchEvent(MotionEvent ev) {
+                mDetector.onTouchEvent(ev);
+                return super.onTouchEvent(ev);
+            }
+        };
+
         lv.setAdapter(new PostAdapter(activity, R.layout.post_item, posts));
 
         return new AlertDialog.Builder(activity).setView(lv).create();
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent2, float v, float v2) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent2, float velocityX, float velocityY) {
+        if (Math.abs(velocityX) > Math.abs(velocityY) + 1000) {
+            getDialog().dismiss();
+        }
+        return false;
     }
 }
